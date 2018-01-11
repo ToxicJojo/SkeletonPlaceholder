3f2a5fdf8dcd5744f39608c7f96b133407a388a4
Johannes Thiel <Johannes.Thiel@hhu.de>
2018-01-11 10:52:19 +0100

    Added text bone

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -13,7 +13,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.sass/,
+        test: /\.scss/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: ['css-loader', 'sass-loader']
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import './style.sass'
+import './style.scss'
